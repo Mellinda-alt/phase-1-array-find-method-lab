d9db8bc650724f5faf017f926d2bb048abb06eec
Mellinda-alt <mellindawambui@gmail.com>
2024-12-04 10:40:19 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+
+function superbowlWin(recordArray) {
+    const winGame = recordArray.find(record => record.result === "W");
+    return winGame ? winGame.year : undefined;
+}
